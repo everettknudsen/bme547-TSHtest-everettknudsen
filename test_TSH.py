0553723c1c839178d017981d95ee21d7a1fc579f
everettknudsen <jek42@duke.edu>
2019-02-13 00:45:06 -0500

    'Added function to test diagnostic function'

--- test_TSH.py
+++ test_TSH.py
@@ -1,1 +1,16 @@
-def test_tsh_diagnosis():
+import pytest
+
+
+@pytest.mark.parametrize('pat_num, expected', [
+    (0, 'normal thyroid function'),
+    (1, 'hyperthyroidism'),
+    (2, 'normal thyroid function'),
+    (3, 'hypothyroidism'),
+])
+def test_tsh_diagnosis(pat_num, expected):
+    from TSH import tsh_diagnosis
+    from TSH import create_person
+    filename = 'test_data.txt'
+    patient = create_person(filename, pat_num)
+    answer = tsh_diagnosis(patient)
+    assert answer == expected
